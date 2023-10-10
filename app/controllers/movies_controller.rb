8402ac9fac794dda80ee0c54174ca07df3b30806
Sara Schoonover <Saramschoon@gmail.com>
2023-10-10 21:24:36 +0000

    refactored

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,35 +1,28 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
-    matching_movies = Movie.all
-
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
       format.html
-      
     end
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id)) 
   end
 
   def create
     @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @the_movie.title = params.fetch(:title)
+    @the_movie.description = params.fetch(:description)
 
     if @the_movie.valid?
       @the_movie.save
